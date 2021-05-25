f2e454c87a9a4fbfd6c39ae3b2b3defff39f8358
Casey Wood <caseywood252@gmail.com>
2021-05-25 03:55:09 +0000

    mutating string to pig latin

---
+++ pig-latin.js
@@ -0,0 +1,18 @@
+const args = process.argv.slice(2)
+
+const pigLatin = function(input) {
+  let finalStr = ''
+  for(let i = 0; i < input.length; i++) {
+    let midStr = input[i]
+    midStr += midStr[0]
+    midStr = midStr.substring(1)
+    midStr += 'ay '
+    finalStr += midStr
+  }
+  return finalStr
+}
+
+console.log(pigLatin(args))
+
+
+

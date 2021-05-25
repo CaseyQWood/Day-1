3d55bcb9a7d27446ca58de0afcd18f02c24662f3
Casey Wood <caseywood252@gmail.com>
2021-05-25 03:57:04 +0000

    added lint inprovements

--- pig-latin.js
+++ pig-latin.js
@@ -1,18 +1,18 @@
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 
 const pigLatin = function(input) {
-  let finalStr = ''
-  for(let i = 0; i < input.length; i++) {
-    let midStr = input[i]
-    midStr += midStr[0]
-    midStr = midStr.substring(1)
-    midStr += 'ay '
-    finalStr += midStr
+  let finalStr = '';
+  for (let i = 0; i < input.length; i++) {
+    let midStr = input[i];
+    midStr += midStr[0];
+    midStr = midStr.substring(1);
+    midStr += 'ay ';
+    finalStr += midStr;
   }
-  return finalStr
-}
+  return finalStr;
+};
 
-console.log(pigLatin(args))
+console.log(pigLatin(args));
 
 
 
